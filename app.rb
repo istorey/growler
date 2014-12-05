7b5ea63ae71825e88eb8b262c6d80d35dd2be2cc
istorey <wrightstoreys@gmail.com>
2014-12-05 16:04:06 -0500

    final

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'pg'
 require 'pry'
 require 'active_record'
 
+require 'sinatra'
+require 'sinatra/reloader'
+
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 ActiveRecord::Base.establish_connection(
@@ -9,6 +12,15 @@ ActiveRecord::Base.establish_connection(
 	host: 'localhost',
 	database: 'growler_db'
 )
+def random_color
+		color_hex = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
+		color = []
+		6.times do
+			color << color_hex.sample
+		end
+		color.join("")
+end
+
 
 class User < ActiveRecord::Base
 	has_many :posts
@@ -20,7 +32,46 @@ class Post < ActiveRecord::Base
 
 end
 
+get '/' do 
+	@posts = Post.all
+
+
+	return erb :index
+end
+
+get '/users' do
+	@users = User.all
+	
+
+	return erb :users
+end
+
+get '/users/:username' do
+	@username = params[:username] 
+	user_id = User.find_by(username: @username).id
+	@user_posts = Post.where(user_id: user_id)
+
+	return erb :userposts
+end
+
+post '/create_user' do
+	@username = params[:username] 
+	@new_user = User.create(username: @username)
+end
+
+post '/create_post' do 
+	username = params[:username]
+	
+	 user = User.find_by(username: username)
+	 user.posts.create(body: params[:body])
+
+end
+
+get '/post/:post_id' do
+	erb :singlepost
+
+end
 
 
 
-binding.pry
+
